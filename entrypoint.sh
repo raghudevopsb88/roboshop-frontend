474462cccd0aa2a04868eae2ec98009c6dee1218
Raghu K <raghuk.vit@gmail.com>
2026-06-05 22:51:14 +0530

    scripts: update 1 file(s) (entrypoint.sh)

--- entrypoint.sh
+++ entrypoint.sh
@@ -30,6 +30,25 @@ mkdir -p /tmp/client_temp /tmp/proxy_temp /tmp/fastcgi_temp /tmp/uwsgi_temp /tmp
 export DNS_RESOLVER="${DNS_RESOLVER:-$(awk '/^nameserver/{print $2; exit}' /etc/resolv.conf)}"
 : "${DNS_RESOLVER:?DNS_RESOLVER is required}"
 
+# Nginx resolver ignores resolv.conf search domains; short Service names must be FQDN.
+K8S_DNS_SUFFIX="${K8S_DNS_SUFFIX:-default.svc.cluster.local}"
+qualify_host() {
+    local host="$1"
+    if [[ "$host" != *.* ]]; then
+        printf '%s.%s' "$host" "$K8S_DNS_SUFFIX"
+    else
+        printf '%s' "$host"
+    fi
+}
+CATALOGUE_HOST=$(qualify_host "$CATALOGUE_HOST")
+USER_HOST=$(qualify_host "$USER_HOST")
+CART_HOST=$(qualify_host "$CART_HOST")
+SHIPPING_HOST=$(qualify_host "$SHIPPING_HOST")
+PAYMENT_HOST=$(qualify_host "$PAYMENT_HOST")
+RATINGS_HOST=$(qualify_host "$RATINGS_HOST")
+ORDERS_HOST=$(qualify_host "$ORDERS_HOST")
+export CATALOGUE_HOST USER_HOST CART_HOST SHIPPING_HOST PAYMENT_HOST RATINGS_HOST ORDERS_HOST
+
 envsubst '${NGINX_PORT} ${DNS_RESOLVER} ${CATALOGUE_HOST} ${CATALOGUE_PORT} ${USER_HOST} ${USER_PORT} ${CART_HOST} ${CART_PORT} ${SHIPPING_HOST} ${SHIPPING_PORT} ${PAYMENT_HOST} ${PAYMENT_PORT} ${RATINGS_HOST} ${RATINGS_PORT} ${ORDERS_HOST} ${ORDERS_PORT}' \
     < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 
